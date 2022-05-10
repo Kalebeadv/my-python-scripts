b0ca4510442b81c695727a39b6d86df21bd814d3
kalebe <kalebeadv66@gmail.com>
2022-05-10 11:04:00 -0300

    first commit

---
+++ search-keyword-in-file.py
@@ -0,0 +1,45 @@
+# Importing BeautifulSoup class from the bs4 module
+from email import charset
+import re
+import codecs
+
+
+def add_(file, readtype, charset):
+
+  arq = codecs.open(file, readtype, charset)
+  readfile = arq.read()
+  arq.close()
+  return readfile
+  
+
+def seach_in_file(readfile, text):
+
+  result = [_ for _ in re.findall(text, readfile)]
+  return result
+
+
+def remove_list_keyword(result, keyreplace):
+
+  formattedlist = []
+
+  for i in result:
+    i = str(i)
+    string = i.replace(keyreplace, "")
+    formattedlist.append(string)
+
+  return formattedlist
+
+
+file = "test.html"
+readtype = "r"
+charset = "utf-8"
+
+readfile = add_(file, readtype, charset)
+result = seach_in_file(readfile, "meta...")
+
+print("old list: " , result)
+
+
+result = remove_list_keyword(result, "meta")
+
+print("new list: ",result)
